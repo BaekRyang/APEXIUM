5d11e39667f55cd7ef114866f1dde49229078e4b
BaekRyang <graywolf147@gmail.com>
2023-08-01 16:58:37 +0900

    System UI Added

---
+++ Assets/__Scripts/Systems/PlayTimer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayTimer : MonoBehaviour
+{
+    public static float    playTime;
+    
+    public        TMP_Text timerTextHm;
+    public        TMP_Text timerTextMs;
+
+    private void Awake()
+    {
+        timerTextHm ??= GameObject.Find("TimerTextHM").GetComponent<TMP_Text>();
+        timerTextMs ??= GameObject.Find("TimerTextMS").GetComponent<TMP_Text>();
+    }
+
+    private       void Update()
+    {
+        playTime += Time.deltaTime*500;
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        timerTextHm.text = $"{(int) playTime / 60:00}:{(int) playTime % 60:00}";
+        timerTextMs.text = $"{(int) (playTime * 100) % 100:00}";
+    }
+    public static void ResetTimer() => playTime = 0;
+    
+}
