ecfee2f40bf7327280a7dea2ff683bca32b4b91e
BaekRyang <graywolf147@gmail.com>
2023-08-13 20:14:38 +0900

    Cinemachine Confiner2D Problem Fix
    Cinemachine Confiner 2D가 PPU/해상도 변경에 제대로 대응하지 못하는 문제 수정 Settings=>SetResoltuon()
    
    DifficultyBar 추가

--- Assets/__Scripts/Network/Client.cs
+++ Assets/__Scripts/Network/Client.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 using ProtoBuf;
 
--- Assets/__Scripts/Settings.cs
+++ Assets/__Scripts/Settings.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Threading.Tasks;
+using Cinemachine;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Settings : MonoBehaviour
 {
-    
+    public TMP_InputField     widthInputField;
+    public TMP_InputField     heightInputField;
+    public TMP_InputField     ppuInputField;
+    public PixelPerfectCamera pixelPerfectCamera;
+
     /// <summary>
     /// PPU와 Height Resolution을 통해 Orthographic Size를 계산
     /// </summary>
@@ -12,5 +19,22 @@ public class Settings : MonoBehaviour
     {
         float _orthographicSize = p_height / 2f / p_ppu;
         return _orthographicSize;
+    }
+
+    public static void SetResolution(int p_width, int p_height)
+    {
+        Screen.SetResolution(p_width, p_height, Screen.fullScreen);
+    }
+
+    public void SetResolution()
+    {
+        Debug.Log("SetResolution");
+        SetResolution(int.Parse(widthInputField.text), int.Parse(heightInputField.text));
+        pixelPerfectCamera.assetsPPU = int.Parse(ppuInputField.text);
+        var cinemachineCamera = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera as CinemachineVirtualCamera;
+        cinemachineCamera.m_Lens.OrthographicSize= CalculateOrthographicSize(pixelPerfectCamera.assetsPPU, int.Parse(heightInputField.text));
+        cinemachineCamera.GetComponent<CinemachineConfiner2D>().InvalidateCache(); //CinemachineConfiner2D의 캐시를 갱신한다.
+
+
     }
 }
