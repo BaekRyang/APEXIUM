e0da7dcec4215debb8af7bb8ac6c485a3ed6ada1
BaekRyang <graywolf147@gmail.com>
2023-09-20 22:23:14 +0900

    #14 Lobby Button/Animation Update
    로비 씬 버튼들 간 이동 관련 작업

--- Assets/__Scripts/LobbyFunctions/Lobby.cs
+++ Assets/__Scripts/LobbyFunctions/Lobby.cs
@@ -11,19 +11,28 @@ public class Lobby : MonoBehaviour
     [SerializeField] private RectTransform characterSelectUI;
     [SerializeField] private RectTransform multiplayerUI;
 
-    private void Awake()
+    [SerializeField] private MMF_Player _currentMMF;
+    [SerializeField] private MMF_Player _previousMMF;
+    
+    private void Start()
     {
         EventBus.Subscribe<ButtonPressedAction>(OnButtonPressed);
+        
+        entranceUI.gameObject.SetActive(true);
+        mainUI.gameObject.SetActive(false);
+        characterSelectUI.gameObject.SetActive(false);
+        // multiplayerUI.gameObject.SetActive(false);
     }
 
     private void OnButtonPressed(ButtonPressedAction _obj)
     {
+        Debug.Log(_obj.buttonName);
         switch (_obj.buttonName)
         {
             case "Entrance":
                 Entrance();
                 break;
-            
+
             case "StartGame":
                 SingleCharacterSelect();
                 break;
@@ -39,7 +48,16 @@ public class Lobby : MonoBehaviour
             case "Quit":
                 QuitGame();
                 break;
+            
+            case "Back":
+                BackToPrevious();
+                break;
         }
+    }
+
+    private void BackToPrevious()
+    {
+        LerpToScene(_currentMMF, _previousMMF);
     }
 
     private void Entrance()
@@ -67,16 +85,31 @@ public class Lobby : MonoBehaviour
         throw new NotImplementedException();
     }
 
-    private async UniTaskVoid LerpToScene(RectTransform _previous, RectTransform _next)
+    private void LerpToScene(RectTransform _current, RectTransform _next)
     {
-        Vector3  _position = transform.position;
+        MMF_Player _previousPlayer = _current.GetComponent<MMF_Player>();
+        MMF_Player _nextPlayer     = _next.GetComponent<MMF_Player>();
         
-        Task _task     = _previous.GetComponent<MMF_Player>().PlayFeedbacksTask(_position);
-        await _task;
-        _previous.gameObject.SetActive(false);
+        LerpToScene(_previousPlayer, _nextPlayer);
+    }
+
+    private async void LerpToScene(MMF_Player _current, MMF_Player _previous)
+    {
+        _currentMMF  = _previous;
+        _previousMMF = _current;
         
-        _next.gameObject.SetActive(true);
-        _task = _next.GetComponent<MMF_Player>().PlayFeedbacksTask(_position);
+        Vector3 _position = transform.position;
+
+        _current.Direction = MMFeedbacks.Directions.BottomToTop;
+        _current.PlayFeedbacks(_position);
+        
+
+        _previous.gameObject.SetActive(true);
+        _previous.Direction = MMFeedbacks.Directions.TopToBottom;
+        Task  _task = _previous.PlayFeedbacksTask(_position);
         await _task;
+        _current.gameObject.SetActive(false);
+        
+
     }
 }
