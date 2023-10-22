0614dc3549192da5b8e29ac7b5a164c5766c185d
BaekRyang <graywolf147@gmail.com>
2023-10-22 22:37:16 +0900

    Boss Sprite Fix

--- Assets/__Scripts/Entity/Enemy/Boss/Logic/RotateToTarget.cs
+++ Assets/__Scripts/Entity/Enemy/Boss/Logic/RotateToTarget.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class RotateToTarget : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private bool      pullTargetToThis;
+    [SerializeField] private Vector3   pullTargetOffset;
 
     private void FixedUpdate()
     {
-        Vector3 targetDir = target.position - transform.position;
+        target.position = pullTargetToThis ? transform.position + pullTargetOffset : target.position;
         
-        float deg = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
-        Quaternion q = Quaternion.AngleAxis(deg + 90, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 20f);
+        Vector3 _targetDir = target.position - transform.position;
+
+        float      _degree     = Mathf.Atan2(_targetDir.y, _targetDir.x) * Mathf.Rad2Deg;
+        Quaternion _quaternion = Quaternion.AngleAxis(_degree - 90, Vector3.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, _quaternion, Time.deltaTime * 20f);
         Debug.DrawLine(transform.position, target.position, Color.red);
-    
     }
 }
