91356fd529d2fc4b176d306b2b75fa7006726620
BaekRyang <graywolf147@gmail.com>
2023-11-11 22:20:59 +0900

    UI_SettingSlider Update
    UI_SettingSlider가 Navigation Overrider로 인해 볼륨 조절은 키 입력을 통해서 처리할 수 없는 문제가 있었는데,
    Summit버튼을 통해서 원하는 슬라이더를 선택하고, 선택된 상테에서 값을 키 입력으로 수정할 수 있는 방식으로 변경함

--- Assets/__Scripts/Enviorments/NavigatorSetter.cs
+++ Assets/__Scripts/Enviorments/NavigatorSetter.cs
@@ -1,60 +1,93 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NavigatorSetter : MonoBehaviour
 {
-    [SerializeField] private Transform    parentLabel;
-    private                  Transform    left, right;
-    private                  Selectable[] _leftSelectables;
-    private                  Selectable[] _rightSelectables;
+    [SerializeField] private Transform        parentLabel;
+    private                  Transform        left, right;
+    private                  List<Selectable> _leftSelectables;
+    private                  List<Selectable> _rightSelectables;
 
     private void Awake()
     {
         left  = transform.Find("Left");
         right = transform.Find("Right");
 
-        _leftSelectables  = left.GetComponentsInChildren<Selectable>();
-        _rightSelectables = right.GetComponentsInChildren<Selectable>();
+        _leftSelectables  = left.GetComponentsInChildren<Selectable>().ToList();
+        _rightSelectables = right.GetComponentsInChildren<Selectable>().ToList();
 
-        if (_leftSelectables.Length + _rightSelectables.Length == 0)
+        List<Selectable> _removeLeft = _leftSelectables.FindAll(_selectable => _selectable is Slider);
+        foreach (Selectable _targetSelectable in _removeLeft) _leftSelectables.Remove(_targetSelectable);
+
+        List<Selectable> _removeRight = _rightSelectables.FindAll(_selectable => _selectable is Slider);
+        foreach (Selectable _targetSelectable in _removeRight) _rightSelectables.Remove(_targetSelectable);
+
+        if (_leftSelectables.Count + _rightSelectables.Count == 0)
         {
             Debug.Log($"<color=yellow>NavigatorSetter</color> : {gameObject.name} has no selectables.");
         }
 
         //레이블 네비게이션의 오른쪽 방향을 요소로 설정
-        Selectable _selectable = parentLabel.GetComponent<Selectable>();
-        Navigation _nav        = _selectable.navigation;
-        _nav.selectOnRight     = _leftSelectables.Length != 0 ? _leftSelectables[0] : null;
-        _selectable.navigation = _nav;
+        {
+            Selectable _selectable = parentLabel.GetComponent<Selectable>();
+            Navigation _nav        = _selectable.navigation;
+            _nav.selectOnRight     = _leftSelectables.Count != 0 ? _leftSelectables[0] : null;
+            _selectable.navigation = _nav;
+        }
 
-        for (int _i = 0; _i < _leftSelectables.Length; _i++)
+        for (int _i = 0; _i < _leftSelectables.Count; _i++)
         {
             Selectable _targetSelectable = _leftSelectables[_i];
             _targetSelectable.navigation = new Navigation
                                            {
                                                mode          = Navigation.Mode.Explicit,
                                                selectOnLeft  = parentLabel.GetComponent<Selectable>(),
-                                               selectOnUp    = _leftSelectables[(_i - 1 + _leftSelectables.Length) % _leftSelectables.Length],
-                                               selectOnDown  = _leftSelectables[(_i     + 1)                       % _leftSelectables.Length],
-                                               selectOnRight = _rightSelectables.Length != 0 ? _rightSelectables[0] : null //있으면 할당하고 없으면 비우기
+                                               selectOnUp    = _leftSelectables[(_i - 1 + _leftSelectables.Count) % _leftSelectables.Count],
+                                               selectOnDown  = _leftSelectables[(_i     + 1)                      % _leftSelectables.Count],
+                                               selectOnRight = _rightSelectables.Count != 0 ? _rightSelectables[0] : null //있으면 할당하고 없으면 비우기
                                            };
         }
 
-        for (int _i = 0; _i < _rightSelectables.Length; _i++)
+        for (int _i = 0; _i < _rightSelectables.Count; _i++)
         {
-            Selectable _targetSelectable = _rightSelectables[_i];
-            _targetSelectable.navigation = new Navigation
+            Selectable _selectable = _rightSelectables[_i];
+            _selectable.navigation = new Navigation
                                            {
                                                mode          = Navigation.Mode.Explicit,
-                                               selectOnLeft  = _leftSelectables.Length != 0 ? _leftSelectables[0] : null,
-                                               selectOnUp    = _rightSelectables[(_i - 1 + _rightSelectables.Length) % _rightSelectables.Length],
-                                               selectOnDown  = _rightSelectables[(_i     + 1)                        % _rightSelectables.Length],
+                                               selectOnLeft  = _leftSelectables.Count != 0 ? _leftSelectables[0] : null,
+                                               selectOnUp    = _rightSelectables[(_i - 1 + _rightSelectables.Count) % _rightSelectables.Count],
+                                               selectOnDown  = _rightSelectables[(_i     + 1)                       % _rightSelectables.Count],
                                                selectOnRight = null
                                            };
         }
 
-        Debug.Log($"<color=yellow>NavigatorSetter</color> : {gameObject.name} is set. Left:{_leftSelectables.Length}, Right:{_rightSelectables.Length}");
+        //삭제한 슬라이더들의 인덱스가 기존의 버튼과 1:1 대응이 되기 때문에
+        //해당 조건을 바탕으로 슬라이더의 위아래 네비게이션을 다른 슬라이더의 버튼으로 설정해준다.
+        SetSliderNavigation(_removeLeft);
+        SetSliderNavigation(_removeRight);
+
+        Debug.Log($"<color=yellow>NavigatorSetter</color> : {gameObject.name} is set. Left:{_leftSelectables.Count}, Right:{_rightSelectables.Count}");
+    }
+
+    /// <summary>
+    /// 슬라이더 전용 네비게이션 설정
+    /// </summary>
+    private void SetSliderNavigation(IReadOnlyList<Selectable> _sliderList)
+    {
+        for (int _i = 0; _i < _sliderList.Count; _i++)
+        {
+            Selectable _selectable = _sliderList[_i];
+            _selectable.navigation = new Navigation
+                                     {
+                                         mode          = Navigation.Mode.Explicit,
+                                         selectOnLeft  = null,
+                                         selectOnUp    = _leftSelectables[(_i - 1 + _leftSelectables.Count) % _leftSelectables.Count],
+                                         selectOnDown  = _leftSelectables[(_i     + 1)                      % _leftSelectables.Count],
+                                         selectOnRight = null
+                                     };
+        }
     }
 }
