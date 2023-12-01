4c54572dfb76597ff247a24ac1e579200a4f3bc5
BaekRyang <graywolf147@gmail.com>
2023-12-01 21:17:32 +0900

    Spring Tile Update

--- Assets/_Tilemap/Spring/Spring.tsx
+++ Assets/_Tilemap/Spring/Spring.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Spring" tilewidth="16" tileheight="16" tilecount="450" columns="25">
- <image source="Spring.png" width="400" height="300"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Spring" tilewidth="16" tileheight="16" tilecount="851" columns="37">
+ <image source="Spring.png" width="592" height="368"/>
 </tileset>
